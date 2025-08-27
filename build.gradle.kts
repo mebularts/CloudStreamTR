ce880a19097da3c0d692c7e80bab0e7d207fbfd8
Mehmet Bulat <113755353+mebularts@users.noreply.github.com>
2025-08-27 07:09:19 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/mebularts/CloudstreamTR")
+        setRepo(System("https://github.com/mebularts/CloudstreamTR")
 
         authors = listOf("mebularts")
     }
