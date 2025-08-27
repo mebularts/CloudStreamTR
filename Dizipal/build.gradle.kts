92469c1d6a820ddeb49fb811e5bd7f39ca0352cc
Mehmet Bulat <113755353+mebularts@users.noreply.github.com>
2025-08-27 06:43:55 +0300

    Update build.gradle.kts

--- Dizipal/build.gradle.kts
+++ Dizipal/build.gradle.kts
@@ -1,18 +1,10 @@
-version = 12
+version = 13
 
 cloudstream {
-    authors     = listOf("mebularts", "muratcesmecioglu")
+    authors     = listOf("mebularts")
     language    = "tr"
-    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("TvSeries", "Movie")
-    iconUrl = "https://dizipal1103.com/assets/images/logo-dpal.svg?V=2"
+    description = "Dizipal sağlayıcısı"
+    status      = 1
+    tvTypes     = listOf("TvSeries", "Movie")
+    iconUrl     = "https://www.google.com/s2/favicons?domain=dizipal1103.com&sz=%size%"
 }
