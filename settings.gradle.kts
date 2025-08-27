27bd1b8bef215dea5e7411e4dd9946bdb3b722b3
Mehmet Bulat <113755353+mebularts@users.noreply.github.com>
2025-08-27 06:47:53 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,19 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-        // Cloudstream Gradle plugin
-        maven("https://raw.githubusercontent.com/recloudstream/cloudstream/master/repo")
-        maven("https://repo.recloudstream.org")
+rootProject.name = "CloudstreamPlugins"
+
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+
+val disabled = listOf<String>("__Temel")
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://raw.githubusercontent.com/recloudstream/cloudstream/master/repo")
-        maven("https://repo.recloudstream.org")
-    }
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-rootProject.name = "CloudStreamTR"
-include(":Dizipal")
+
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
