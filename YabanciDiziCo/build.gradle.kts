0043a9d373c30e0cbb3f36a5a71701d1669049c8
Mehmet Bulat <113755353+mebularts@users.noreply.github.com>
2025-08-27 07:55:36 +0300

    Create build.gradle.kts

---
+++ YabanciDiziCo/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 13
+
+cloudstream {
+    authors     = listOf("mebularts")
+    language    = "tr"
+    description = "YabancıDizi.Co sağlayıcısı"
+    status      = 1
+    tvTypes     = listOf("TvSeries", "Movie")
+    iconUrl     = "https://yabancidizi.so/favicon.ico"
+}
